7295b6fbb6d22004bf1ecdeeef3be45804f5201c
Alex Soto <apsoto@gmail.com>
2014-07-28 11:45:47 -0700

    initial working code

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php
+<?
 
-echo "Hello World!";
+define('WURL_CLIENT_KEY', "26eff9b0ad7ff9a5163ca9d7d20d09ad61b7200a42c52232de59c7e78b0cbb03");
+define('WURL_SECRET_KEY', "0d5f95b1c18e355fe915a8297c083d84646d7367f59140715bbacfac35b56d77");
+
+require_once('wurl-api.php');
+$wurl_api = new WurlApi(constant("WURL_CLIENT_KEY"), constant("WURL_SECRET_KEY"));
+
+/* Get the id we are requesting */
+#$id = $wurl_api->bridge_get($_REQUEST['id']);
+
+
 
 ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Bridge API Query Demo</title>
+</head>
+<body>
+  <p>This is a quick demo on the bridge API.</p>
+  <p><?= var_dump($wurl_api->bridge_get($_REQUEST['id']));?></p>
+</body>
+</html>
---
+++ wurl-api.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/** Sample Wurl API Access using http */
+class WurlApi {
+  var $client_id;
+  var $client_secret;
+
+  const WURL_API_ENDPOINT = "http://api.wurl.com/api";
+
+  function WurlApi($client_id, $client_secret) {
+    $this->client_id;
+    $this->client_secret;
+
+  }
+
+  function bridge_get($id) {
+    $opts = array(
+      "http"=>array(
+        "method"=>"GET"
+      )
+    );
+    $context = stream_context_create($opts);
+
+    $url = self::WURL_API_ENDPOINT."/search?type=episode&fields=bridge_tags&q=".urlencode($id)."&appid=26eff9b0ad7ff9a5163ca9d7d20d09ad61b7200a42c52232de59c7e78b0cbb03&secret=0d5f95b1c18e355fe915a8297c083d84646d7367f59140715bbacfac35b56d77";
+    $file = file_get_contents($url,false, $context);
+    return json_decode($file);
+  }
+
+}
+?>
