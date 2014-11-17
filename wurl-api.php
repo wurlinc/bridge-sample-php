b1a63bfe20b4507e6596c0592ba7f46fb82d6a7a
Karl Forsman <kforsman@wurl.com>
2014-11-17 14:11:53 -0800

    updating code to reflect new api endpoint

--- wurl-api.php
+++ wurl-api.php
@@ -18,10 +18,11 @@ class WurlApi {
     );
     $context = stream_context_create($opts);
 
-    $url = self::WURL_API_ENDPOINT."/search?type=episode&fields=bridge_tags&q=".urlencode($id)."&access_token={$this->access_token}";
+    $url = self::WURL_API_ENDPOINT."/bridge_assets/".urlencode($id)."?access_token={$this->access_token}";
     $file = file_get_contents($url,false, $context);
     return json_decode($file);
   }
 
 }
 ?>
+
